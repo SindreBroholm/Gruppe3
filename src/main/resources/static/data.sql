9bcbe6777acf184fb91b40744311e8ab82c53b7c
SindreBroholm <sindreset@gmail.com>
2020-10-12 12:00:56 +0200

    Created and populatet tabels with dummi data for testing. -Sindre

---
+++ src/main/resources/static/data.sql
@@ -0,0 +1,28 @@
+insert into user(Email, Firstname, Lastname, Password) values
+('Sindreset@gmail.com', 'Sindre Broholm', 'Sæther', 'Sindre123'),
+('marit_jp@hotmail.com', 'Marit', 'Pedersen', 'Marit123'),
+('fredrsan@gmail.com', 'Fredrik', 'Sandbakk', 'Fredrik123'),
+('helgerannem@gmail.com', 'Helge', 'Rannem', 'Helge123');
+
+insert into board(name) values
+('Fastis test Board'),
+('Fastis Subtest Board');
+
+insert into membership_type(name) values
+('admin'),
+('leader'),
+('member'),
+('follower');
+
+insert into user_role(user_id, board_id, membership_type, numb_of_members) values
+(1, 1, 'leader', 1),
+(1, 2, 'follower', 1),
+(2, 1, 'member', 1),
+(2, 2, 'follower', 1),
+(3, 1, 'follower', 1),
+(3, 2, 'leader', 1),
+(4, 1, 'member', 1),
+(4, 2, 'follower', 1);
+
+insert into events(board_id, message, datetime_from, datetime_to, datetime_created, event_type) values
+(1, 'Hælg', '2020-10-16 16:00:00', '2020-10-18 23:59:59', '2020-10-12 12:00:00', 'follower');
---
+++ src/main/resources/static/schema.sql
@@ -0,0 +1,62 @@
+create schema fastis;
+
+create table user(
+    Id int auto_increment unique,
+    Email varchar(320) not null unique,
+    Firstname varchar(100) not null,
+    Lastname varchar(100) not null,
+    Password varchar(300) not null,
+    primary key (Id)
+);
+
+drop table board;
+drop table user_role;
+drop table events;
+drop table notification;
+
+
+create table board(
+    Id int unique auto_increment,
+    name varchar(100) not null unique,
+    primary key (Id)
+);
+
+create table user_role(
+    User_Id int not null,
+    Board_Id int not null,
+    membership_Type varchar(56) not null,
+    Numb_Of_Members int not null,
+    primary key (User_Id, Board_Id),
+    FOREIGN KEY (User_Id) references user(Id) ON DELETE CASCADE,
+    foreign key (Board_Id) references board(Id) ON DELETE CASCADE,
+    foreign key (membership_Type) references membership_Type(name)
+);
+
+create table events(
+    Id int unique auto_increment,
+    Board_Id int not null unique,
+    Message varchar(1000) not null,
+    Datetime_From datetime not null,
+    Datetime_To datetime not null,
+    Datetime_Created datetime not null,
+    Event_Type varchar(56) not null,
+    foreign key (Event_Type) references membership_Type(name),
+    foreign key (Board_Id) references board(Id) ON DELETE CASCADE
+);
+
+create table notification
+(
+    Id int auto_increment unique not null,
+    Board_Id int unique not null,
+    Message varchar(5000) not null,
+    Datetime_Created datetime not null,
+    notification_Type varchar(56) not null,
+    primary key (Id),
+    foreign key (Board_Id) references board(Id) ON DELETE CASCADE,
+    foreign key (notification_Type) references membership_Type(name)
+);
+
+create table  membership_Type(
+    name varchar(56) not null,
+    primary key (name)
+);
