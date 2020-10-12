408552778a5fdab3578ac5fd955b338f1a862d94
Helge Rannem <helgerannem@gmail.com>
2020-10-12 09:19:52 +0200

    first commit

---
+++ src/main/java/com/fastis/FastisApplication.java
@@ -0,0 +1,13 @@
+package com.fastis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastis/FastisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
